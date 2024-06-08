b501cc254086bb0ac5adca950f4854abedc94459
Psychopatz <bpatrickoliverb@gmail.com>
2024-06-08 20:32:16 +0800

    Implement Youtube Search

--- Backend/api/main.py
+++ Backend/api/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from youtube_search import YoutubeSearch
 
 app = FastAPI()
 
 @app.get("/")
 async def health_check():
     return {"message": "Karayouke API is up and running!"}
+
+
+@app.get("/search")
+async def search(query: str):
+    results = YoutubeSearch(query, max_results=10).to_dict()
+    return {"results": results}
+
